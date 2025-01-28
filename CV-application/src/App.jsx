fdd12a3eb83e25fce19197be716cdab37c3ed4c7
Nagul75 <nagulk697@gmail.com>
2025-01-28 16:04:19 +0530

    Add custom font and center the page

--- CV-application/src/App.jsx
+++ CV-application/src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./components/Header"
-
+import "./App.css"
 function App() {
   return(
-      <Header></Header>
+    <div className="page-container">
+      <div className="page">
+        <Header></Header>
+      </div>
+    </div>
   )
 }
 
